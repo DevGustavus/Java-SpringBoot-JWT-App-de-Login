7d187686f0c31f40ee7902f9a43ad39cf7682393
Gustavo Machado Pontes <gustavusinvictus@gmail.com>
2025-04-24 02:08:33 -0300

    atualizando controller do user

--- src/main/java/com/DevGustavus/login_auth_api/controllers/UserController.java
+++ src/main/java/com/DevGustavus/login_auth_api/controllers/UserController.java
@@ -1,15 +1,33 @@
 package com.DevGustavus.login_auth_api.controllers;
 
+import com.DevGustavus.login_auth_api.models.User;
+import com.DevGustavus.login_auth_api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping
-    public ResponseEntity<String> getUser(){
-        return ResponseEntity.ok("sucesso!");
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable String id) {
+        return userRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
